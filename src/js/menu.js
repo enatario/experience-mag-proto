aa98e265100ef04739eee255736a0cac0fe5ea41
enatario <elaina.natario@gmail.com>
2018-05-10 19:00:04 -0400

    fancy menu stuff

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,12 @@
-import { select, addClass, removeClass, hasClass } from "./utils/dom"
-
-const menuBtn = select("[data-menu-button]")
-const menuSection = select("[data-menu-section]")
-const bodyEl = select("body")
-const menuClass = "is-revealed"
-const bodyClass = "menu-is-revealed"
+import { select, selectAll, addClass, removeClass, hasClass } from "./utils/dom"
 
 const revealMenu = () => {
+
+  const menuBtn = select("[data-menu-button]")
+  const menuSection = select("[data-menu-section]")
+  const bodyEl = select("body")
+  const menuClass = "is-revealed"
+  const bodyClass = "menu-is-revealed"
 
   menuBtn.addEventListener("click", () => {
     const isRevealed = hasClass(menuSection, menuClass)
@@ -21,4 +21,26 @@ const revealMenu = () => {
     }
   })
 }
-export default revealMenu
+
+const hoverImage = () => {
+
+  const menuLinks = selectAll("[data-menu-link]")
+  const menuBg = select("[data-menu-bg]")
+
+  menuLinks.forEach((link, index) => {
+    
+    index = index + 1
+
+    link.addEventListener("mouseover", () => {
+      menuBg.style.backgroundImage = `url(/assets/images/menu-${index}.jpg)`
+    })
+
+  })
+
+}
+
+const init = () => {
+  revealMenu()
+  hoverImage()
+}
+export default init
