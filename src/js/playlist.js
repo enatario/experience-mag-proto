99673dae56c74bffbb981fd1a054522adcb338df
enatario <elaina.natario@gmail.com>
2018-05-14 10:05:08 -0400

    fix pl bug

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -4,54 +4,58 @@ import inView from "in-view"
 const body = select("body")
 const playlistNav = select("[data-playlist-nav]")
 const playlistList = select("[data-playlist-list]")
+const pageId = body.getAttribute("data-page-type")
 
 const playlist = () => {
 
-  const pageId = body.getAttribute("data-page-type")
+  const headlines = selectAll("[data-article-hed]")
+
+  headlines.forEach((hed, index) => {
+    const article = hed.closest("article")
+    const hedText = hed.innerHTML
+    const slug = hedText
+                    .replace(/\s+/g, "-")
+                    .replace(/\./g,"")
+                    .replace(/\’/g,"")
+                    .replace(/\!/g,"")
+                    .toLowerCase()
+
+    article.setAttribute("id", slug)
+    index = index + 1
+
+    const playlistInner =
+      `<li class="playlist-nav__item">
+        <p class="playlist-nav__overline">Story ${index}</p>
+        <a class="playlist-nav__link" href="#${slug}">${hedText}</a>
+      </li>`
+
+    return playlistList.innerHTML += playlistInner
+
+  })
+}
+
+const init = () => {
 
   if (pageId == "playlist") {
 
-    const headlines = selectAll("[data-article-hed]")
+    playlist()
 
-    headlines.forEach((hed, index) => {
-      const article = hed.closest("article")
-      const hedText = hed.innerHTML
-      const slug = hedText
-                      .replace(/\s+/g, "-")
-                      .replace(/\./g,"")
-                      .replace(/\’/g,"")
-                      .replace(/\!/g,"")
-                      .toLowerCase()
+    const playlistIsHidden = playlistNav.hasAttribute("hidden")
 
-      article.setAttribute("id", slug)
-      index = index + 1
+    inView("[data-article-content]")
+      .on("enter", () => {
+        if (playlistIsHidden) {
+          playlistNav.removeAttribute("hidden")
+        }
+      })
 
-      const playlistInner =
-        `<li class="playlist-nav__item">
-          <p class="playlist-nav__overline">Story ${index}</p>
-          <a class="playlist-nav__link" href="#${slug}">${hedText}</a>
-        </li>`
-
-      return playlistList.innerHTML += playlistInner
-
-    })
+    inView("[data-site-footer]")
+      .on("enter", () => {
+        if (!playlistIsHidden) {
+          playlistNav.setAttribute("hidden", "")
+        }
+      })
   }
-
-  const playlistIsHidden = playlistNav.hasAttribute("hidden")
-
-  inView("[data-article-content]")
-    .on("enter", () => {
-      if (playlistIsHidden) {
-        playlistNav.removeAttribute("hidden")
-      }
-    })
-
-  inView("[data-site-footer]")
-    .on("enter", () => {
-      if (!playlistIsHidden) {
-        playlistNav.setAttribute("hidden", "")
-      }
-    })
 }
 
-export default playlist
+export default init
