1a3f5cc9f8d9e8f1f299c2f4ec51b0e65067b78e
enatario <elaina.natario@gmail.com>
2018-05-29 07:21:17 -0400

    playlist behavior

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -3,38 +3,15 @@ import { select, selectAll, addClass, removeClass, hasClass } from "./utils/dom"
 const body = select("body")
 const pageId = body.getAttribute("data-page-type")
 const playlistNav = select("[data-playlist-nav]")
-const playlistList = select("[data-playlist-list]")
 const plBtns = selectAll("[data-article-toggle-btn]")
 const showAllBtn = select("[data-article-toggle-all]")
+const navIsHidden = playlistNav.hasAttribute("hidden")
+const articles = selectAll("[data-article-toggle-content]")
 
-const createPlaylistNav = () => {
 
-  const headlines = selectAll("[data-article-hed]")
-
-  headlines.forEach((hed, index) => {
-    const article = hed.closest("article")
-    const hedText = hed.innerHTML
-    const slug = hedText
-                    .replace(/\s+/g, "-")
-                    .replace(/\./g,"")
-                    .replace(/\’/g,"")
-                    .replace(/\!/g,"")
-                    .toLowerCase()
-
-    article.setAttribute("id", slug)
-    index = index + 1
-
-    const playlistInner =
-      `<li class="playlist-nav__item">
-        <p class="playlist-nav__overline">Story ${index}</p>
-        <a class="playlist-nav__link" href="#${slug}">${hedText}</a>
-      </li>`
-
-    return playlistList.innerHTML += playlistInner
-  })
-}
 
 const revealArticle = () => {
+
   plBtns.forEach(btn => {
     const id = btn.getAttribute("data-article-toggle-btn")
     const article = select(`[data-article-toggle-content=${id}]`)
@@ -42,51 +19,37 @@ const revealArticle = () => {
     btn.addEventListener("click", () => {
       article.removeAttribute("hidden")
       addClass(btn, "is-hidden")
+
+      if (navIsHidden) {
+        playlistNav.removeAttribute("hidden")
+      }
     })
   })
 }
 
-const revealAll = () => {
+const collapseAll = () => {
   showAllBtn.addEventListener("click", () => {
-    const allArticlesCollapsed = hasClass(showAllBtn, "playlist-is-collapsed")
 
-    if (allArticlesCollapsed) {
-      removeClass(showAllBtn, "playlist-is-collapsed")
-      showAllBtn.innerHTML = "Collapse all stories"
+    playlistNav.setAttribute("hidden", "")
 
-      const articles = selectAll("[data-article-toggle-content]")
-      articles.forEach(article => {
-        article.removeAttribute("hidden")
-      })
+    articles.forEach(article => {
+      const id = article.getAttribute("data-article-toggle-content")
+      const btn = select(`[data-article-toggle-btn=${id}]`)
 
-      plBtns.forEach(btn => {
-        addClass(btn, "is-hidden")
-      })
-    }
-
-    else {
-      addClass(showAllBtn, "playlist-is-collapsed")
-      showAllBtn.innerHTML = "Show all stories"
-
-      const articles = selectAll("[data-article-toggle-content]")
-      articles.forEach(article => {
+      if (!article.hasAttribute("hidden")) {
         article.setAttribute("hidden", "")
-      })
-
-      plBtns.forEach(btn => {
         removeClass(btn, "is-hidden")
-      })
-    }
+      }
+
+    })
   })
 }
 
 const init = () => {
 
   if (pageId == "playlist") {
-
-    createPlaylistNav()
     revealArticle()
-    revealAll()
+    collapseAll()
   }
 }
 
